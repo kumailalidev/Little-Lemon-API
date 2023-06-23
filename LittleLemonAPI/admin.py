d7c16130806798dff5a74c6ee3b19143f291ec6c
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-06-23 20:51:51 +0500

    Added models for menu items and categories

--- LittleLemonAPI/admin.py
+++ LittleLemonAPI/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from LittleLemonAPI.models import Category, MenuItem
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(MenuItem)
--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Product category."""
+
+    slug = models.SlugField()
+    title = models.CharField(max_length=255)
+
+
+class MenuItem(models.Model):
+    """Menu item"""
+
+    title = models.CharField(max_length=255, db_index=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
+    featured = models.BooleanField(db_index=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
