824eaf68353237b385872cb29a6c9639fb386d35
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-06-24 00:33:05 +0500

    Added views for categories and menu items endpoints

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,1 +1,7 @@
+from django.urls import path
+from LittleLemonAPI import views
 
+urlpatterns = [
+    path("categories/", views.CategoriesView.as_view(), name="categories"),
+    path("menu-items/", views.MenuItemsView.as_view(), name="menu_items"),
+]
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from .models import (
+    Category,
+    MenuItem,
+)
+from .serializers import (
+    CategorySerializer,
+    MenuItemSerializer,
+)
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAdminUser,
+)
+from rest_framework.response import Response
+
+
+class CategoriesView(generics.ListCreateAPIView):
+    """View for displaying categories"""
+
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method != "GET":
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    """View for displaying menu items."""
+
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    search_fields = ["category__title"]
+    ordering_fields = ["price", "inventory"]
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method != "GET":
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    """View for displaying, updating, and deleting single menu item."""
+
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method != "GET":
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
