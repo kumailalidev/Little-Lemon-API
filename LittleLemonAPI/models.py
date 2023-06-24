af56dd7f49153f790f939fe6eddc7fdcac4244ea
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-06-24 19:58:50 +0500

    Fixed incorrect field name in OrderItem model

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -58,4 +58,4 @@ class OrderItem(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
     class Meta:
-        unique_together = ("order", "menuitems")
+        unique_together = ("order", "menuitem")
