db8fb38523d02acb0a724198cb6160628cc88488
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-06-24 16:56:09 +0500

    Added models and serializer for orders and order items.

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -6,6 +6,8 @@ from .models import (
     Category,
     MenuItem,
     Cart,
+    Order,
+    OrderItem,
 )
 
 
@@ -42,3 +44,21 @@ class CartSerializer(serializers.ModelSerializer):
         model = Cart
         fields = ["user", "menuitem", "unit_price", "quantity", "price"]
         extra_kwargs = {"price": {"read_only": True}}
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    """Serializer for order item."""
+
+    class Meta:
+        model = OrderItem
+        fields = ["order", "menuitem", "quantity", "price"]
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    """Serializer for order."""
+
+    orderitem = OrderItemSerializer(many=True, read_only=True, source="order")
+
+    class Meta:
+        model = Order
+        fields = ["id", "user", "delivery_crew", "status", "date", "total", "orderitem"]
